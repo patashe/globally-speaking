8953b91a6e708e288e7ba0765a8ea982bab2466d
Patrick Ashe <pat@ican.ie>
2016-08-07 21:25:12 +0100

    first commit

---
+++ src/javascripts/app.js
@@ -0,0 +1,10 @@
+//import './modules'
+
+const jQuery = require('jquery');
+
+global.jQuery = jQuery;
+global.$ = jQuery;
+
+const bootstrap = require('bootstrap-sass')
+
+console.log(`app.js has loaded!`)
